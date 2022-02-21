b4bf7ae77d15c4733262ebb734ce9365f69f8f04
Afridi Hasan <ahasan35@myseneca.ca>
2022-02-20 21:02:07 -0500

    initial commit

---
+++ data-service.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const fs = require("fs");
+const { resolve } = require("path");
+
+var departments = [];
+var employees = [];
+
+module.exports.initialize = function () {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./data/employees.json", "utf8", (err, data) => {
+      if (err) reject(err);
+      employees = JSON.parse(data);
+      resolve();
+    });
+    fs.readFile("./data/departments.json", "utf8", (err, data2) => {
+      if (err) reject(err);
+      departments = JSON.parse(data2);
+      resolve();
+    });
+  });
+};
+
+module.exports.getAllEmployees = function () {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(employees);
+    } catch (err) {
+      console.log("error", err);
+    }
+  });
+};
+
+module.exports.getManagers = function () {
+  return new Promise((resolve, reject) => {
+    try {
+      reject(employees);
+    } catch (err) {
+      console.log("error", err);
+    }
+  });
+};
+
+module.exports.getDepartments = function () {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(departments);
+    } catch (err) {
+      console.log("error", err);
+    }
+  });
+};
+
+module.exports.addEmployee = function (employeeData) {
+  return new Promise((resolve, reject) => {
+    employeeData.isManager = employeeData.isManager ? true : false;
+    try {
+      employees.push(employeeData);
+      employeeData.employeeNum = employees.length + 1;
+      resolve();
+    } catch (err) {
+      console.log("error==>", err);
+    }
+  });
+};
+
+module.exports.getEmployeesByStatus = function (status) {
+  return new Promise((resolve, reject) => {
+    var m_status = [];
+    for (var i = 0; i < employees.length; i++) {
+      if (employees[i].status == status) {
+        m_status.push(employees[i]);
+      }
+    }
+    if (m_status.length == 0) reject("Error!");
+    resolve(m_status);
+  });
+};
+
+module.exports.getEmployeesByDepartment = function (department) {
+  return new Promise((resolve, reject) => {
+    var m_depart = [];
+    for (var i = 0; i < employees.length; i++) {
+      if (employees[i].department == department) {
+        m_depart.push(employees[i]);
+      }
+    }
+    if (m_depart.length == 0) reject("Error!");
+    resolve(m_depart);
+  });
+};
+
+module.exports.getEmployeesByManager = function (manager) {
+  return new Promise((resolve, reject) => {
+    var m_manager = [];
+    for (var i = 0; i < employees.length; i++) {
+      if (employees[i].employeeManagerNum == manager) {
+        m_manager.push(employees[i]);
+      }
+    }
+    if (m_manager.length == 0) reject("Error!");
+    resolve(m_manager);
+  });
+};
+
+module.exports.getEmployeeByNum = function (num) {
+  return new Promise((resolve, reject) => {
+    var m_employee = [];
+    for (var i = 0; i < employees.length; i++) {
+      if (employees[i].employeeNum == num) {
+        //     //console.log(employee);
+        m_employee.push(employees[i]);
+      }
+    }
+    if (m_employee.length == 0) reject("Error!");
+    resolve(m_employee);
+  });
+};
---
+++ server.js
@@ -0,0 +1,157 @@
+/*********************************************************************************
+* WEB322 – Assignment 03
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Afridi Hasan Student ID: 125653196 Date: 20th February 2020
+*
+* Online (Heroku) Link: ________________________________________________________
+*
+********************************************************************************/ 
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+const multer = require("multer");
+var app = express();
+var path = require("path");
+const bodyParser = require("body-parser");
+var dataService = require("./data-service.js");
+const { resolve } = require("path");
+
+app.use(express.static('public')); // static middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    console.log("Express http server listening on: " + HTTP_PORT);
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT); // .then(() => { app.listen(HTTP_PORT); })
+
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));
+    },
+  });
+  
+
+  const upload = multer({ storage: storage });
+  
+
+  app.get("/about", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+  });
+  
+
+  app.get("/employees", (req, res) => {
+    if (req.query.status) {
+      dataService
+        .getEmployeesByStatus(req.query.status)
+        .then((employees) => {
+          res.json(employees);
+        })
+        .catch();
+    } else if (req.query.department) {
+      dataService
+        .getEmployeesByDepartment(req.query.department)
+        .then((data) => {
+          res.json(data);
+        })
+        .catch();
+    } else if (req.query.employeeManagerNum) {
+      dataService
+        .getEmployeesByManager(req.query.employeeManagerNum)
+        .then((data) => {
+          res.json(data);
+        })
+        .catch();
+    } else if (req.query.employeeNum) {
+      dataService
+        .getEmployeeByNum(req.query.employeeNum)
+        .then((data) => {
+          res.json(data);
+        })
+        .catch();
+    } else
+      dataService
+        .getAllEmployees()
+        .then((employees) => {
+          res.json(employees);
+        })
+        .catch((err) => {
+          res.send(err);
+        });
+  });
+  
+  app.get("/managers", (req, res) => {
+    let managers = [];
+    dataService
+      .getManagers()
+      .then((employees) => {
+        for (let i = 0; i < employees.length; i++) {
+          if (employees[i].isManager) {
+            managers.push(employees[i]);
+            res.json(employees);
+          }
+        }
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
+  
+  app.get("/departments", function (req, res) {
+    dataService
+      .getDepartments()
+      .then((departments) => {
+        res.json(departments);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
+  
+  app.get("/employees/add", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/addEmployee.html"));
+  });
+  
+  app.post("/employees/add", function (req, res) {
+    dataService
+      .addEmployee(req.body)
+      .then(() => {
+        res.redirect("/employees");
+      })
+      .catch(function (err) {
+        console.log("An error was encountered: " + err);
+      });
+  });
+  
+  app.get("/images/add", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/addImage.html"));
+  });
+  
+  app.post("/images/add", upload.single("imageFile"), (req, res) => {
+    res.redirect("/images");
+  });
+  
+  app.get("/images", function (req, res) {
+    fs.readdir("./public/images/uploaded", function (err, items) {
+      var parsedItems = JSON.stringify({ images: items });
+  
+      res.send(parsedItems);
+    });
+  });
+//////////////////
+
+// error page (did not try to make it custom. maybe next time)
+app.use((req, res) => {
+    res.status(404).send("404 Page Not Found");
+});
+
+// step 5 ( didnt add app.listen() ) see line 27
+dataService
+    .initialize()
+    .catch((err) => { console.error(err); });
